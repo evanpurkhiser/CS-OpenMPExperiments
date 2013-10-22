9ee958c0349c1939a7347b319ab797c7f38586e1
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-22 17:36:38 -0400

    Read in the real product too so we can check it

--- omp-matrix-mutliplcation/matrix.c
+++ omp-matrix-mutliplcation/matrix.c
@@ -7,6 +7,7 @@
 
 #define MATRIX_1 "matrix1.txt"
 #define MATRIX_2 "matrix2.txt"
+#define PRODUCT  "product.txt"
 
 #define MATRIX_SIZE 16
 
@@ -15,11 +16,14 @@ int main(int argc, char *argv[])
 	puts("\e[0;34m==>\e[0m Reading in values from the matrix files...");
 
 	double matrix1[MATRIX_SIZE][MATRIX_SIZE],
-	       matrix2[MATRIX_SIZE][MATRIX_SIZE];
+	       matrix2[MATRIX_SIZE][MATRIX_SIZE],
+	       real_product[MATRIX_SIZE][MATRIX_SIZE],
+	       calc_product[MATRIX_SIZE][MATRIX_SIZE];
 
 	{
 		FILE *matrix1_file = fopen(MATRIX_1, "r"),
-		     *matrix2_file = fopen(MATRIX_2, "r");
+		     *matrix2_file = fopen(MATRIX_2, "r"),
+		     *product_file = fopen(PRODUCT,  "r");
 
 		for (int i = 0; i < MATRIX_SIZE; ++i)
 		{
@@ -27,10 +31,13 @@ int main(int argc, char *argv[])
 			{
 				fscanf(matrix1_file, "%lf", &matrix1[i][j]);
 				fscanf(matrix2_file, "%lf", &matrix2[i][j]);
+				fscanf(product_file, "%lf", &real_product[i][j]);
 			}
 		}
+
 		fclose(matrix1_file);
 		fclose(matrix2_file);
+		fclose(product_file);
 	}
 
 	return 0;
