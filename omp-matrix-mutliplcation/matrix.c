4055575a0af7cb0fce2169e2167dfea052cd105a
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-22 17:11:23 -0400

    Add empty program
    The program does included headers we will (probably) need however

---
+++ omp-matrix-mutliplcation/matrix.c
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/time.h>
+
+#define MATRIX_1 "matrix1.txt"
+#define MATRIX_2 "matrix2.txt"
+
+int main(int argc, char *argv[])
+{
+	puts("\e[0;34m==>\e[0m Reading in values from the matrix files...");
+
+	return 0;
+}
