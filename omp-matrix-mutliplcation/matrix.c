fa8c9a0bf6ea88cee484332257c0ce401f3d9a30
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-22 17:33:54 -0400

    Read in the two matrixes

--- omp-matrix-mutliplcation/matrix.c
+++ omp-matrix-mutliplcation/matrix.c
@@ -8,9 +8,30 @@
 #define MATRIX_1 "matrix1.txt"
 #define MATRIX_2 "matrix2.txt"
 
+#define MATRIX_SIZE 16
+
 int main(int argc, char *argv[])
 {
 	puts("\e[0;34m==>\e[0m Reading in values from the matrix files...");
+
+	double matrix1[MATRIX_SIZE][MATRIX_SIZE],
+	       matrix2[MATRIX_SIZE][MATRIX_SIZE];
+
+	{
+		FILE *matrix1_file = fopen(MATRIX_1, "r"),
+		     *matrix2_file = fopen(MATRIX_2, "r");
+
+		for (int i = 0; i < MATRIX_SIZE; ++i)
+		{
+			for (int j = 0; j < MATRIX_SIZE; ++j)
+			{
+				fscanf(matrix1_file, "%lf", &matrix1[i][j]);
+				fscanf(matrix2_file, "%lf", &matrix2[i][j]);
+			}
+		}
+		fclose(matrix1_file);
+		fclose(matrix2_file);
+	}
 
 	return 0;
 }
